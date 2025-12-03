02926e08abdfc1a879e68081694ae7bfc8e46d02
Muhammet Ozdemir <65421785+mr-ozdemir@users.noreply.github.com>
2025-12-03 20:48:03 +0300

    Create config.py

---
+++ BI-RADS Classification/config.py
@@ -0,0 +1,20 @@
+
+DATA_DIR = './kendi_meme_kanseri_veri_seti_klasor_yolu'
+
+
+NUM_EPOCHS = 50
+BATCH_SIZE = 32
+LEARNING_RATE = 0.001
+COUNTER = 1  
+
+
+N_CHANNELS = 3
+N_CLASSES = 4
+IMG_SIZE = 260
+
+
+SEED = 42
+
+
+MEAN = [0.485, 0.456, 0.406]
+STD = [0.229, 0.224, 0.225]
